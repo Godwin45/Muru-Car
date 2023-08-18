056321ed617426ac4222673a3fc062a50ac92c7c
Godwin45 <godwinmuthomi12@gmail.com>
2023-08-18 16:31:19 +0300

    data cleaning complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mlProject import logger
 import os
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from mlProject.pipeline.stage_02_data_cleaning import DataCleaningPipeline
+from mlProject.pipeline.stage_02_data_cleaning import DataCleaningPipeline
 # from mlProject.pipeline.stage_03_data_validation import DataValidationTrainingPipeline
 # from mlProject.pipeline.stage_04_data_transformation import DataTransformationTrainingPipeline
 # from mlProject.pipeline.stage_05_model_trainer import ModelTrainerTrainingPipeline
@@ -13,6 +13,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Cleaning stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_cleaning = DataCleaningPipeline()
+   data_cleaning.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,7 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
-                                            # DataCleaningConfig,
+                                            DataCleaningConfig,
                                             # DataValidationConfig,
                                             # DataTransformationConfig,
                                             # ModelTrainerConfig,
@@ -33,3 +33,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_cleaned_data_config(self) -> DataCleaningConfig:
+        config = self.config.data_cleaning
+
+        create_directories([config.root_dir])
+
+        data_cleaning_config = DataCleaningConfig(
+            root_dir=config.root_dir,
+            cleaned_data=config.cleaned_data,
+            unzip_data_dir = config.unzip_data_dir,
+        )
+
+        return data_cleaning_config
