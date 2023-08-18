60a4a5c144d1d045d01b25cd005c8844009495d9
Godwin45 <godwinmuthomi12@gmail.com>
2023-08-18 16:50:11 +0300

    data validation complete

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mlProject import logger
 import os
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_cleaning import DataCleaningPipeline
-# from mlProject.pipeline.stage_03_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_validation import DataValidationTrainingPipeline
 # from mlProject.pipeline.stage_04_data_transformation import DataTransformationTrainingPipeline
 # from mlProject.pipeline.stage_05_model_trainer import ModelTrainerTrainingPipeline
 # from mlProject.pipeline.stage_06_model_evaluation import ModelEvaluationTrainingPipeline
@@ -23,6 +23,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_cleaning = DataCleaningPipeline()
    data_cleaning.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
