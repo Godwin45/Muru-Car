da3ac57c86c0143601c6790d16b6e5588b14b808
Godwin45 <godwinmuthomi12@gmail.com>
2023-08-19 19:44:19 +0300

    model training complete

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_cleaning import DataCleaningPipeline
 from mlProject.pipeline.stage_03_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_04_data_transformation import DataTransformationTrainingPipeline
-# from mlProject.pipeline.stage_05_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_05_model_trainer import ModelTrainerTrainingPipeline
 # from mlProject.pipeline.stage_06_model_evaluation import ModelEvaluationTrainingPipeline
 
 
@@ -42,6 +42,16 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -4,7 +4,7 @@ from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataCleaningConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            # ModelTrainerConfig,
+                                            ModelTrainerConfig,
                                             # ModelEvaluationConfig
                                             )
 
@@ -73,3 +73,21 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        schema = self.schema.TARGET_COLUMN
+        classifier_params = self.params
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name=config.model_name,
+            target_column=schema.name,
+            classifier_params=classifier_params
+        )
+
+        return model_trainer_config
